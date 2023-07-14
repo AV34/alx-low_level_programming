28690c28a4f203b687908e95669374fa9453e6fe
av34 <machebepatrick@gmail.com>
2023-07-14 07:31:02 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Start of the program
+ *
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+	int w;
+	
+	for (w = 0; w < 10; w++)
+		printf("%d", w);
+	putchar('\n');
+	return (0);
+}
