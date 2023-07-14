512f83ba26714d9905e0a89c36c57277f68132f7
av34 <machebepatrick@gmail.com>
2023-07-14 07:50:19 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Start of the program
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char q;
+	q = 'z';
+	while (q >= 'a') {
+		putchar(q);
+		q--;
+	}
+	putchar('\n');
+	return (0);
+}
