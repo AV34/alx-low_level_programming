ac9864872379710ff8a54d4dffd571b609eb6e5b
av34 <machebepatrick@gmail.com>
2023-07-14 11:09:00 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - Prints all possible combinations of a pair of
- * two digit numbers, without any repetition.
-(*
- * Return: 0 on success.
+ * main - start of the program
+ *
+ * Return: 0 (Success)
  */
 
 int main(void)
