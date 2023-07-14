e6ee894c3cbb4f1f07a2ae46b9bd0be7553a8ddf
av34 <machebepatrick@gmail.com>
2023-07-14 09:45:02 -0700

    task *

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - This is the begining of the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char d;
+	int e;
+	e = 0;
+	d = 'a';
+	while ( e < 10) {
+		putchar(e + '0');
+		e++;
+	}
+	while (d <= 'f') {
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
