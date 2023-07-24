1a0e02de7ce50de500ecbbe70886a4c6f3284a38
av34 <machebepatrick@gmail.com>
2023-07-24 11:45:18 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts2 - funt to print only one char out of two
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	in longi = 0;
+	int t = 0;
+	char *y = str;
+	int 0;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+	}
+		_putchar(str[0]);
+	}
+	}
+	_putchar('\n');
+}
