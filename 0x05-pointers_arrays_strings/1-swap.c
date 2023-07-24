312db91d329d08d829cd5d76b126f639f3c65de4
av34 <machebepatrick@gmail.com>
2023-07-24 10:44:39 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - value swap of int
+ * @a: Int to swap
+ * @b: Int to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int m;
+	
+	m = *a;
+	*a = *b;
+	*b = m;	
+}
