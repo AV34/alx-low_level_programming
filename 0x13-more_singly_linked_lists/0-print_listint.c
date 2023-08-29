e656fe4b3fb4c2a036f44d3ec7835439e0849bca
AV34 <machebepatrick@gmail.com>
2023-08-29 07:02:48 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,17 +5,16 @@
  * @h: indentifier for linked list of type listint_t
  * Return: num of nodes
  */
- 
+
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
 
 	while (h)
 {
- printf("%d\n", h->n);
- num++;
- h = h->next;
- }
-
- return (num);
+	printf("%d\n", h->n);
+	num++;
+	h = h->next;
+}
+	return (num);
 }
